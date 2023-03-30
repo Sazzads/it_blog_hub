862e90a9dd317edf1506e039f1febe7ed137f7a8
Sazzads <sazzadtk@gmail.com>
2023-03-31 03:42:48 +0600

    blog card added

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -4,13 +4,16 @@ import './Header.css'
 const Header = () => {
     return (
         <div>
-            <nav className="navbar navbar-light bg-light justify-content-between">
-                <a className="navbar-brand">IT Blog Hub</a>
-                <div>
-                    <a className='text-decoration-none text-secondary me-1' href="/home">Home</a>
-                    <a className='text-decoration-none text-secondary me-1' href="/blog">Blog</a>
-                    <a className='text-decoration-none text-secondary me-1' href="/about">About</a>
-                    <a href=""><img className='nav-image' src="../../../images/nav_dp.jpg" alt="" /></a>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand">IT Blog Hub</a>
+                    <div className="d-flex justify-content-between">
+                        <a href="/home">Home</a>
+                        <a href="/about">About</a>
+                        <a href="/blog">Blog</a>
+                        <a href="/blog">Blog</a>
+                        <a href=""><img className='nav-image' src="../../../images/nav_dp.jpg" alt="" /></a>
+                    </div>
                 </div>
             </nav>
         </div>
--- src/Component/SingleBlog/SingleBlog.jsx
+++ src/Component/SingleBlog/SingleBlog.jsx
@@ -1,3 +1,5 @@
+import { faCoffee, faTag, faTags, faUserTag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react';
 import './SingleBlog.css'
 
@@ -8,22 +10,25 @@ const SingleBlog = ({ movie }) => {
                 <div className="w-100">
                     <img className='w-100' src={movie.cover_image} alt="" />
                 </div>
-                <div className="d-flex justify-content-between ">
-                    <div className='d-flex '>
-                        <img className='author-dp' src={movie.author_image} alt="" />
-                        <div className='text-start ms-2'>
+                <div className='row mt-2'>
+                    <div className='col d-flex align-items-center'>
+                            <img className='w-25 rounded-5' src={movie.author_image} alt="" />
+                        <div className='col text-start ms-2'>
                             <h6>{movie.author_name}</h6>
                             <p>{movie.date}</p>
                         </div>
                     </div>
-                    <div className='d-flex'>
-                    <small className='d-flex'>{movie.read_time} min read</small>
-                    <p>font awesome</p>
+
+                    <div className='col d-flex justify-content-end'>
+                        <small className='read-time'>{movie.read_time} mins read</small>
+                        <button className='btn btn-light'><FontAwesomeIcon icon={faTag} /></button>
                     </div>
                 </div>
-                <h2>dark</h2>
-                <p>Lorem, ipsum.</p>
-                <button className='btn btn-info w-75 m-auto'>book now</button>
+                <h3 className='text-start mt-2'>{movie.title}</h3>
+                <h6 className='text-start mt-2 fw-lighter'>{movie.hashTag}</h6>
+                <a className='text-start mt-2 text-decoration-underline' href="">Mark as read</a>
+
+
             </div>
         </div>
     );
